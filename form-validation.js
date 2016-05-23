cd91583da403c7629e6122e6fc1a4d12403268df
Evan Sharp <dumbdrum@gmail.com>
2016-05-23 13:37:04 -0400

    Update form-validation.js

--- form-validation.js
+++ form-validation.js
@@ -1,20 +1,70 @@
-app.directive('form', function () {
-    return {
-        require: 'form',
-        restrict: 'E',
-        link: function( scope , element , attributes ){
-            var $element = $(element);
-            $element.on('submit', function(e) {
-                $element.find('.ng-pristine').removeClass('ng-pristine').addClass('ng-dirty');
-                var form = scope[ attributes.name ];
-                angular.forEach( form , function ( formElement , fieldName ) {
-                    if ( fieldName[0] === '$' ) return;
-                    formElement.$pristine = false;
-                    formElement.$dirty = true;
-                },this);
-                form.$setDirty();
-                scope.$apply();
-            });
-        }
-    };
-});
+const moduleName = 'validation.form';
+
+function form() {
+  return {
+    require: 'form',
+    restrict: 'E',
+    link: {
+      pre: function (scope, element, attributes, formCtrl) {
+
+        attributes.$set('novalidate', 'novalidate');
+
+        var validateForm = form => {
+          angular.forEach(form, (formElement, fieldName) => {
+            if (fieldName[0] === '$') {
+              return;
+            }
+            if (!angular.isFunction(formElement.$setTouched)) {
+              return validateForm(formElement);
+            }
+
+            var needsToValidate = !(formElement.$dirty && formElement.$touched) || formElement.$invalid;
+
+            formElement.$setDirty();
+            formElement.$setTouched();
+
+            if (needsToValidate) {
+              formElement.$validate();
+            }
+          });
+          form.$setDirty();
+          
+          if(angular.isFunction(form.$setSubmitted)) {
+            form.$setSubmitted();
+          }
+        };
+
+        element.on('submit', (e) => {
+          validateForm(formCtrl);
+          scope.$apply();
+
+          if (attributes.ngSubmit && !formCtrl.$submitting && (formCtrl.$valid || attributes.allowInvalidSubmission)) {
+            //run ng-submit
+            formCtrl.$submitting = scope.$eval(attributes.ngSubmit, {$event: e});
+
+            // ng-submit function should return a promise if it wants to prevent multiple simultaneous submissions of the same form
+            if (angular.isObject(formCtrl.$submitting) && angular.isFunction(formCtrl.$submitting.finally)) {
+              formCtrl.$submitting.finally(resp => {
+                formCtrl.$submitting = false;
+                return resp;
+              });
+            } else {
+              formCtrl.$submitting = false;
+            }
+          } else if (!attributes.ngSubmit && attributes.allowInvalidSubmission) {
+            return;
+          }
+
+          e.preventDefault();
+          e.stopImmediatePropagation();
+          return false;
+        });
+      }
+    }
+  };
+}
+
+angular.module(moduleName, [])
+  .directive('form', form);
+
+export default moduleName;
